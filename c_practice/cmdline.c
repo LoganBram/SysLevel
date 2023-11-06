7207a804406aec03a4d15dc5cb94e6c074e32e28
Logan Bramwell (20LWKB) <20lwkb@LINUX3.caslab.queensu.ca>
2023-11-06 12:14:01 -0500

    median for 3 values working

--- c_practice/cmdline.c
+++ c_practice/cmdline.c
@@ -1,6 +1,18 @@
 // save as cmdline.c
 #include <stdio.h>
 #include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+    float float_a = *((float *)a);
+    float float_b = *((float *)b);
+
+    if (float_a == float_b) return 0;
+    else if (float_a < float_b) return -1;
+    else return 1;
+}
+
+
+
 int main(int argc, char *argv[]) {
     if (argc < 2){
         printf("%d\n", 0);
@@ -12,11 +24,13 @@ int main(int argc, char *argv[]) {
         printf("%f\n", median);
         return 0;
     } else if (argc == 4){
-        for (int i = 0; i < argc; i++) {
-        char *str = argv[i];
-        double val = atof(str);
-        printf("argv[%d] : %f\n", i, val);
-        return 0; }
+        float numbers[3];
+        for (int i = 1; i < argc; i++) {
+            numbers[i-1] = atof(argv[i]);
+            printf("argv[%d] : %f\n", i, numbers[i-1]);
+        }
+        qsort(numbers, 3, sizeof(float), compare);
+        printf("%f\n", numbers[1]);
     }
      else {
         exit(1);
