d6f5d8d66b9dae981059ec3cb1f703b2790ef014
Logan Bramwell (20LWKB) <20lwkb@LINUX3.caslab.queensu.ca>
2023-12-09 14:41:13 -0500

    comma count works

--- assi/A6/testfolder/test.c
+++ assi/A6/testfolder/test.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 #include <string.h>
 
 int main() {
-   //iterate through string and count commas
-   //while(*str)
+    char str[] = "hello, world, this is a test string";
+    int count = 0;
 
-   //create array of comma count +1
+    // Iterate through string and count commas
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ',') {
+            count++;
+        }
+    }
 
-   //use strtok(string,",")
+    printf("Number of commas in the string: %d\n", count);
+
+
+    //use strtok(string,",")
+   printf(str);
 
    //for loop of comma count +1
     //token = strtok(NULL, ",")
     //conver token using atoi
     //store in array at i of for loop
-   
+    // Further processing can be done here if needed, like using strtok
+    // But remember, strtok modifies the original string
+
+    return 0;
 }
 
